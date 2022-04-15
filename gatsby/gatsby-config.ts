051a9280302c18f5fdf234e8a65d78e3843fba20
Dominik Borchert <dominik.borchert@arvato-scs.com>
2022-04-15 16:58:19 +0200

    Connected gatsby to strapi API

--- gatsby/gatsby-config.ts
+++ gatsby/gatsby-config.ts
@@ -1,11 +1,24 @@
-import type { GatsbyConfig } from "gatsby"
+import type { GatsbyConfig } from 'gatsby'
 
 const config: GatsbyConfig = {
-  siteMetadata: {
-    title: `My Gatsby Site`,
-    siteUrl: `https://www.yourdomain.tld`,
-  },
-  plugins: [],
+	siteMetadata: {
+		title: `My Gatsby Site`,
+		siteUrl: `https://www.yourdomain.tld`,
+	},
+	plugins: [
+		{
+			resolve: 'gatsby-source-strapi',
+			options: {
+				apiURL: 'http://strapi:1337',
+				collectionTypes: [
+					{
+						singularName: 'page',
+					},
+				],
+				queryLimit: 1000,
+			},
+		},
+	],
 }
 
 export default config

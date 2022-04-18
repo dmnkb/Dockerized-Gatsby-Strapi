259d290f73476da65f0fb90fef55814101ce10cd
Dominik Borchert <dominik.borchert@arvato-scs.com>
2022-04-18 16:41:49 +0200

    Fix test

--- gatsby/cronjob.js
+++ gatsby/cronjob.js
@@ -1,5 +1,7 @@
 const cp = require('child_process')
 
+// TODO: Retry logic! Strapi takes a while to build
+
 console.log(`Start rebuilding Gatsby`)
 const child = cp.exec('yarn clean && yarn build')
 child.stdout.pipe(process.stdout)
